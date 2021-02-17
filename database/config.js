c5b42e9d6e68b08ee4d88462c89f7fc6a4800955
Alejandro <alex.9939@hotmail.com>
2021-02-17 14:52:38 -0600

    partial changes for mongo integration

---
+++ database/config.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async() => {
+    
+    try {
+
+        await mongoose.connect(process.env.MONGODBCONN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            
+        });
+        
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error a la hora de iniciar la BD');
+    }
+
+
+}
+
+
+module.exports = {
+    dbConnection
+}
